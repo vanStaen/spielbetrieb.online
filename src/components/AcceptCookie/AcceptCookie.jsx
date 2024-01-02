3c59b324ffc4570bb634f9892b20ddc429649b55
vanStaen <clement.vanstaen@gmail.com>
2024-01-02 10:39:05 +0100

    Cookie in german + mobile behavior

--- src/components/AcceptCookie/AcceptCookie.jsx
+++ src/components/AcceptCookie/AcceptCookie.jsx
@@ -1,50 +1,51 @@
 import React, { useEffect } from "react";
-import { Button } from 'antd';
-import { CloseOutlined } from '@ant-design/icons';
+import { useTranslation } from "react-i18next";
+import { Button } from "antd";
+import { CloseOutlined } from "@ant-design/icons";
 import { pageStore } from "../../store/pageStore";
 
-import './AcceptCookie.less';
+import "./AcceptCookie.less";
 
 export const AcceptCookie = () => {
-    const windowInnerWidth = window.innerWidth;
+  const windowInnerWidth = window.innerWidth;
+  const { t } = useTranslation();
 
-    useEffect(() => {
-        const elementCookieContainer = document.getElementById('cookiecontainer');
-        console.log('elementCookieContainer', elementCookieContainer)
-        setTimeout(() => {
-            if (windowInnerWidth > 675) {
-                elementCookieContainer.style.bottom = '0px';
-            } else {
-                elementCookieContainer.style.top = '0px';
-            }
-        }, "500");
-    }, [])
+  useEffect(() => {
+    const elementCookieContainer = document.getElementById("cookiecontainer");
+    console.log("elementCookieContainer", elementCookieContainer);
+    setTimeout(() => {
+      elementCookieContainer.style.bottom = "0px";
+    }, "500");
+  }, []);
 
-    const handleCloseClick = (value) => {
-        if (value) {
-            pageStore.setAllowCookie(true);
-        }
-        const elementCookieContainer = document.getElementById('cookiecontainer');
-        if (windowInnerWidth > 675) {
-            elementCookieContainer.style.right = '-500px';
-        } else {
-            elementCookieContainer.style.top = '-250px';
-        }
+  const handleCloseClick = (value) => {
+    if (value) {
+      pageStore.setAllowCookie(true);
     }
+    const elementCookieContainer = document.getElementById("cookiecontainer");
+    if (windowInnerWidth > 675) {
+      elementCookieContainer.style.right = "-500px";
+    } else {
+      elementCookieContainer.style.right = `-${windowInnerWidth + 10}px`;
+    }
+  };
 
-    return (
-        <>
-            <div className="cookie__container" id="cookiecontainer">
-                <div className="cookie__title">
-                    🍪 This website uses cookies.
-                </div>
-                <div className="cookie__body">
-                    We use cookies to analyze website traffic and optimize your website experience.
-                    By accepting our use of cookies, your data will be aggregated with all other user data.
-                </div>
-                <Button className="cookie__button" onClick={() => handleCloseClick(true)}>Accept</Button>
-                <CloseOutlined className="cookie__closebutton" onClick={() => handleCloseClick(false)} />
-            </div>
-        </>
-    )
-} 
+  return (
+    <>
+      <div className="cookie__container" id="cookiecontainer">
+        <div className="cookie__title">🍪 {t("legal.cookiesTitle")}</div>
+        <div className="cookie__body">{t("legal.cookiesDesc")}</div>
+        <Button
+          className="cookie__button"
+          onClick={() => handleCloseClick(true)}
+        >
+          {t("legal.accept")}
+        </Button>
+        <CloseOutlined
+          className="cookie__closebutton"
+          onClick={() => handleCloseClick(false)}
+        />
+      </div>
+    </>
+  );
+};
